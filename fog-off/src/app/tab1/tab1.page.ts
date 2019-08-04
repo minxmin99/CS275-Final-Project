93b4cb776cfae00f996bef2db5c818bdd30ebe3d
Phan Quân <qmp23@drexel.edu>
2019-08-04 00:07:18 -0400

    Add change name function on tab 1

--- fog-off/src/app/tab1/tab1.page.ts
+++ fog-off/src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ModalController, AlertController } from '@ionic/angular';
+// import { ModalPagePage } from '../modal-page/modal-page.page';
+// import { AlertPagePage } from '../alert-page/alert-page.page';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,50 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private modal: ModalController, 
+    private alert:AlertController
+    ) {}
 
+  // async openModal() {
+  //   const nameModal = await this.modal.create({
+  //     component: ModalPagePage
+  //   });
+  //   return await nameModal.present();
+  // }
+
+  username: string = "Player 1"; 
+
+  async openAlert() {
+    const nameAlert = await this.alert.create({
+      header: "Edit Name", 
+      inputs: [
+        {
+          name: "new_username",
+          type: "text",
+          placeholder: "Type new username"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "danger",
+          handler: () => {
+            console.log("Cancal ops");
+          }
+        }, {
+          text: "OK",
+          handler: (data) => {
+
+            // TODO: Check new name legitimacy
+
+            console.log("New name")
+            console.log(data.new_username)
+            this.username = data.new_username;
+          }
+        }
+      ]
+    });
+    await nameAlert.present();
+  }
 }
