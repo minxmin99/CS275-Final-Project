99fcd4f31610e13864ea8d15655e45968e8e1aa4
DESKTOP-FUPOQS4\chis.tea <tl669@drexel.edu>
2019-08-29 09:58:48 -0400

    acheivements functions

--- fog-off/src/app/tab3/tab3.page.ts
+++ fog-off/src/app/tab3/tab3.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
+@Component({selector: 'app-tab3',
+templateUrl: 'tab3.page.html',
+styleUrls: ['tab3.page.scss']
+})
+export class NewsFeedPage {
+  items = [];
+
+  constructor() {
+    for (let i = 0; i < 30; i++) {
+      this.items.push( this.items.length );
+    }
+  }
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      for (let i = 0; i < 30; i++) {
+        this.items.push( this.items.length );
+      }
+
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 500);
+  }
+
+}
+ 
 @Component({
   selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  templateUrl: 'tab3.page.html'
 })
 export class Tab3Page {
-
-  constructor() {}
-
+  galleryType = 'regular';
+  constructor(public navCtrl: NavController) { }
 }
